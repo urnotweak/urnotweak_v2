c5d4e47f593c8ea955f67fcf86c0a9738e68a619
soyoon <barcardi26@gmail.com>
2023-07-27 10:10:15 +0900

    start

---
+++ Project/Front-end/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import StartForm from "./components/SimulationForm/StartForm/StartForm";
+import "./App.css";
+
+function App() {
+  return <StartForm />;
+}
+
+export default App;
---
+++ Project/Front-end/src/components/SimulationForm/StartForm/StartForm.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./StartForm.css";
+
+const StartForm = () => {
+  return (
+    <div className="android---5-1">
+      <div className="status-bar">
+        <p className="text-1">12:45</p>
+        <div className="icons">
+          <div className="m-3-/-2-4-/wifi">
+            <img src="" />
+            <img src="" />
+          </div>
+          <img src="" />
+          <img src="" />
+        </div>
+      </div>
+      <div className="frame-2-6-0-8-1-3-8">
+        <p className="text-5">약해지지마</p>
+        <img src="" />
+      </div>
+      <div className="frame-6-2">
+        <div className="group-6-3">
+          <p className="text-7">02-1234-56**</p>
+        </div>
+        <div className="group-5-4">
+          <button className="buttons-/-dark">
+            <div className="frame-3-8" />
+          </button>
+          <div className="ellipse-4-4" />
+          <img src="" />
+          <p className="text-9">밀어서 통화하기</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StartForm;
