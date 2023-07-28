ad0f5e592b43e76d4670bf5cd9ba20388a29bec9
young eun <duddms0190@naver.com>
2023-07-28 15:23:38 +0900

    Feat: Navigation

--- Project/Front-end/src/components/Nav/Nav.js
+++ Project/Front-end/src/components/Nav/Nav.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Menu } from "./Menu";
-// import "./style.css";
+import { Link } from "react-router-dom";
+import "./Nav.css";
 
 export const Nav = () => {
   return (
-    <div className="nav">
-      <div className="element">약해지지마</div>
-      <Menu className="m-menu" color="black" />
-    </div>
+      <div className="navbar">
+        <Link to="/" className="element" style={{ textDecoration: "none" }}>약해지지마</Link>
+        <Menu className="m-menu" color="white" />
+      </div>
   );
 };
 
