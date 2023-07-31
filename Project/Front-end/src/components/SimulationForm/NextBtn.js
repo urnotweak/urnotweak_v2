8a6f3c8a769ece5a93b75b054622bfcfeeaee6d0
soyoon <barcardi26@gmail.com>
2023-07-31 19:17:57 +0900

    Feat : Simul / StoryForm

--- Project/Front-end/src/components/SimulationForm/NextBtn.js
+++ Project/Front-end/src/components/SimulationForm/NextBtn.js
@@ -2,20 +2,7 @@ import { useCallback } from "react";
 import NxtBtnImg from "../../assets/group-45.svg";
 import "./NextBtn.css";
 const NextBtn = () => {
-  const onGroupIconClick = useCallback(() => {
-    // Please sync "story3 -3" to the project
-  }, []);
-
-  return (
-    <div className="android-88">
-      <img
-        className="android-88-child"
-        alt=""
-        src={NxtBtnImg}
-        onClick={onGroupIconClick}
-      />
-    </div>
-  );
+  return <img className="next-btn" alt="" src={NxtBtnImg} />;
 };
 
 export default NextBtn;
