28c7e920278bb555079821894cd47f1ea6e3fa6f
youngeun <duddms0190@naver.com>
2023-08-01 16:35:01 +0900

    Feat: Add Menubar (스크롤 내리면 안나오는 부분 고쳐야함)

---
+++ Project/Front-end/src/pages/Menu/Menu.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './Menu.css';
+
+export const MenuBar = ({closebtn}) => {
+  return (
+    <div className="bg">
+        <div onClick={closebtn}>
+            <svg
+                className="close"
+                fill="none"
+                height="28"
+                viewBox="0 0 24 24"
+                width="28"
+                xmlns="http://www.w3.org/2000/svg"
+                >
+                <path
+                className="path"
+                d="M17.5 8.055L16.44 7L12.25 11.195L8.055 7L7 8.055L11.195 12.25L7 16.44L8.055 17.5L12.25 13.31L16.44 17.5L17.5 16.44L13.31 12.25L17.5 8.055Z"
+                fill="black"
+                />
+            </svg>
+        </div>
+        <div className="items">
+            <div className="item"><Link to="/" className="text-wrapper">Home </Link></div>
+            <div className="item"><Link to="/simul" className="text-wrapper">Simulation </Link></div>
+            <div className="item"><Link to="/test" className="text-wrapper">Test </Link></div>
+            <div className="item"><Link to="/" className="text-wrapper">AboutUs </Link></div>
+        </div>
+    </div>
+  );
+};
+
+export default MenuBar;
