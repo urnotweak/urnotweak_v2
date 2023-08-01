057980d9ca8ff33c2be091841727154f8434112b
soyoon <barcardi26@gmail.com>
2023-08-01 23:09:58 +0900

    Feat : simul result-form

--- Project/Front-end/src/components/SimulationForm/SelectForm/SelecctForm.js
+++ Project/Front-end/src/components/SimulationForm/SelectForm/SelecctForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import SelectBox from "./SelectBox";
 import StartAnd from "../StartForm/StartAnd";
+import ResultForm from "../ResultForm/ResultForm";
+import Nav from "components/Nav/Nav";
 import "./SelectForm.css";
 
 const SelectForm = () => {
@@ -9,12 +11,15 @@ const SelectForm = () => {
     setSelectedIndex(index);
   };
 
-  if (selectedIndex == 1) {
+  if (selectedIndex === 1) {
     return <StartAnd />;
+  } else if (selectedIndex === 2) {
+    return <ResultForm />;
   }
 
   return (
     <div className="simul-">
+      <Nav />
       <div className="parent">
         <div className="simul-txt">시뮬레이션</div>
         <div className="simul-txt2">시나리오 선택</div>
---
+++ Project/Front-end/src/components/SimulationForm/StoryForm/HandForm.js
@@ -0,0 +1,51 @@
+import { useCallback } from "react";
+import "./HandForm.css";
+const HandForm = () => {
+  const onM324menuIconClick = useCallback(() => {
+    // Please sync "Android - 4" to the project
+  }, []);
+
+  return (
+    <div className="android-82">
+      <div className="status-bar">
+        <div className="div">12:45</div>
+        <div className="icons">
+          <img className="m324wifi-icon" alt="" src="/m324wifi.svg" />
+          <img
+            className="m324wifi-icon"
+            alt=""
+            src="/m324signal-cellular.svg"
+          />
+          <img className="m324wifi-icon" alt="" src="/m324battery-full.svg" />
+        </div>
+      </div>
+      <div className="status-bar">
+        <div className="div">약해지지마</div>
+        <img
+          className="m324menu-icon"
+          alt=""
+          src="/m324menu.svg"
+          onClick={onM324menuIconClick}
+        />
+      </div>
+      <div className="httpslottiefilescomanimat-parent">
+        <img
+          className="httpslottiefilescomanimat-icon"
+          alt=""
+          src="/httpslottiefilescomanimationsshieldupuejjv2msnp@2x.png"
+        />
+        <div className="group-parent">
+          <img className="group-child" alt="" src="/group-64.svg" />
+          <img
+            className="httpslottiefilescomanimat-icon1"
+            alt=""
+            src="/httpslottiefilescomanimationsshieldupuejjv2msnp1@2x.png"
+          />
+          <div className="div2">드래그하여 내려주세요</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HandForm;
