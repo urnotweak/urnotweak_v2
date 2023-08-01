535eef7117e4c7283d05b97a71e28ad9064b18f4
KimJiHong <kjh95044@naver.com>
2023-08-01 09:46:07 +0900

    Remove: Remove all

--- src/components/SimulationForm/NextBtn.js
+++
@@ -1,8 +0,0 @@
-import { useCallback } from "react";
-import NxtBtnImg from "../../assets/group-45.svg";
-import "./NextBtn.css";
-const NextBtn = () => {
-  return <img className="next-btn" alt="" src={NxtBtnImg} />;
-};
-
-export default NextBtn;
