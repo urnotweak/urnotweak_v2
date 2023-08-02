92c65a1587ebdc8d29c0ba37f565e7b08c86a026
hyoinjeong <hyoin.jeong3@gmail.com>
2023-08-02 16:32:49 +0900

    Fix: CORS 수정

--- Project/Back-end/src/main/java/com/ssafy/drug/controller/TestController.java
+++ Project/Back-end/src/main/java/com/ssafy/drug/controller/TestController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("test")
 public class TestController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
