ffbf274583cd95a5ee2dcd1da3b903716590461e
soyoon <barcardi26@gmail.com>
2023-08-06 15:39:12 +0900

    Feat: simul - add monoForm

--- Project/Front-end/src/components/SimulationForm/StoryForm/MonoForm.js
+++ Project/Front-end/src/components/SimulationForm/StoryForm/MonoForm.js
@@ -1,14 +1,12 @@
 import "./MonoForm.css";
 import NextBtn from "../NextBtn";
-const MonoForm = ({onNext}) => {
+const MonoForm = ({onNext,text}) => {
   return (
     <div className="mono">
       <NextBtn onClick={onNext}/>
       <div className="mono-wrapper">
         <div className="mono-txt">
-          <p className="p">너무 힘든데...</p>
-          <p className="p">&nbsp;</p>
-          <p className="p">딱 한 번만 더 해볼까?</p>
+          <p className="p">{text}</p>
         </div>
       </div>
     </div>
