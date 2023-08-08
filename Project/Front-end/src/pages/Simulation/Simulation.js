c21db41b7d433bc4ff7d7a1165cd6c155177565c
soyoon <barcardi26@gmail.com>
2023-08-08 15:38:34 +0900

    Faet : simul -add ai link

--- Project/Front-end/src/pages/Simulation/Simulation.js
+++ Project/Front-end/src/pages/Simulation/Simulation.js
@@ -1,21 +1,12 @@
 import React from "react";
 import "./Simulation.css";
 import { Nav } from "components/Nav/Nav";
-import StartAnd from "components/SimulationForm/StartForm/StartAnd";
-import NextBtn from "components/SimulationForm/NextBtn";
-import StartTxt from "components/SimulationForm/StartForm/StartTxt";
 import SelectForm from "components/SimulationForm/SelectForm/SelecctForm";
-import VideoForm from "components/SimulationForm/StoryForm/VideoForm";
-import SelectTxt from "components/SimulationForm/StoryForm/SelectTxt";
 export const Simulation = () => {
   return (
     <div className="simul">
       <Nav />
-      {/* <SelectTxt /> */}
-      {/* <Nav className="nav-instance" /> */}
       <SelectForm />
-      {/* <StartAnd /> */}
-      {/* <VideoForm /> */}
     </div>
   );
 };
