e79f47572766f1d1fd255dbaefe195712b721e64
hyoinjeong <hyoin.jeong3@gmail.com>
2023-08-09 17:34:48 +0900

    Feat: Flask 서버 API를 이용해 마약후사진 생성

--- Project/Front-end/src/components/AI/UploadForm/aiUpload.js
+++ Project/Front-end/src/components/AI/UploadForm/aiUpload.js
@@ -2,9 +2,11 @@ import React, {useState, useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 import ImageSlider from "components/common/BeforeAfterImg/ImageSlider";
 import './aiUpload.css'
+import axios from 'axios';
 
 export const AI = () => {
   const [imageSrc, setImageSrc] = useState(null);
+  const [selectedFiles, setSelectedFiles] = useState(null);
   const navigate = useNavigate();
 
   // 사진 올렸을 때 실행
@@ -20,7 +22,8 @@ export const AI = () => {
 
     return new Promise((resolve) => { 
         reader.onload = () => {	
-            setImageSrc(reader.result || null); // 파일의 컨텐츠
+          setImageSrc(reader.result || null); // 파일의 컨텐츠
+          setSelectedFiles(file);
             resolve();
         };
     });
@@ -29,9 +32,32 @@ export const AI = () => {
   // 생성하기 눌렀을때
   const createResult = () => {
     // 사진 보내서 결과 받아오기 과정 필요
+        // forData 생성
+    const formData = new FormData();
+    // Todo : api로 주소 받아와야함
+    let apiUrl = `http://835e-34-23-71-131.ngrok-free.app`
+    const url = apiUrl+"/AI"
+    formData.append('file', selectedFiles);
 
+        axios({
+          method: 'POST',
+          url: url,
+          headers: {
+            'Content-Type': 'multipart/form-data', // 이것 필수
+          },
+          data: formData,
+          responseType: "blob",
+        }).then((response) => {
+          console.log(response);
+          const result = URL.createObjectURL(response.data)
+          navigate('/ai/result', {
+            state: {
+              aiImg: result,
+            }
+          });
+        });
 
-    navigate('/ai/result');
+    // navigate('/ai/result');
   }
 
 
