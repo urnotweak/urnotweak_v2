be8c873c5de8b411ef05a3d25aa70ce1eed7bdfb
HyoinJeong <hiamy0107@naver.com>
2023-08-10 11:31:45 +0900

    Feat: 마약 후 AI 사진 화면에 표시

--- Project/Front-end/src/components/AI/ResultForm/aiResult.js
+++ Project/Front-end/src/components/AI/ResultForm/aiResult.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from "react";
-import {useNavigate} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { BottomMenu } from "components/common/BottomMenu/BottomMenu";
-import './aiResult.css'
-import { useLocation } from 'react-router-dom';
+import "./aiResult.css";
+import { useLocation } from "react-router-dom";
 
 export const AI = () => {
   const [imageSrc, setImageSrc] = useState(null);
@@ -10,55 +10,28 @@ export const AI = () => {
   const navigate = useNavigate();
 
   const goUpload = () => {
-    navigate('/ai/upload');
-  }
+    navigate("/ai/upload");
+  };
 
-  // const {state} = useLocation();	// 2번 라인
-  
-  //   console.log('user 값이 설정됨');
-  // const { aiFile } = state;	// 3번 라인
-
-   // 1. useLocation 훅 취득
+  // navigator에서 보낸 마약전후 이미지 가져오기
   const location = useLocation();
-
-  console.log(location.state);
-
-  // 2. location.state 에서 파라미터 취득
   const aiFile = location.state.aiImg;
-  console.log(typeof aiFile)
-  console.log(aiFile);
 
-    // const reader = new FileReader();
-    // reader.readAsDataURL(aiFile);
-  
-    // reader.onload = () => {	
-    //   setImageSrc(reader.result || null); // 파일의 컨텐츠
-    // };
   useEffect(() => {
     setImageSrc(aiFile || null);
-      
-    },[])
-    
-
-
-
-  // 1. useLocation 훅 취득
-  // const location = useLocation();
-
-  // 2. location.state 에서 파라미터 취득
-  // const aiFile = location.state.aiImg;
-
+  }, []);
 
   return (
     <div className="upload">
-      <img 
-        className="uploadimg"
-        src={imageSrc} 
-        />
-      <div className="btn" onClick={goUpload}>다시해보기</div>
+      <div className="resultimg">
+        <img className="uploadimg" src={imageSrc} />
+      </div>
+      <div className="btn" onClick={goUpload}>
+        다시해보기
+      </div>
       <BottomMenu></BottomMenu>
     </div>
   );
 };
-    
+
 export default AI;
--- Project/Front-end/src/components/AI/UploadForm/aiUpload.js
+++ Project/Front-end/src/components/AI/UploadForm/aiUpload.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from "react";
-import {useNavigate} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import ImageSlider from "components/common/BeforeAfterImg/ImageSlider";
-import './aiUpload.css'
-import axios from 'axios';
+import "./aiUpload.css";
+import axios from "axios";
 
 export const AI = () => {
   const [imageSrc, setImageSrc] = useState(null);
@@ -13,77 +13,85 @@ export const AI = () => {
   const onUpload = (e) => {
     // 에러처리
     console.log(e.target.files);
-    if(e.target.files.length == 0) return;
+    if (e.target.files.length == 0) return;
 
     // file -> url 변환
     const file = e.target.files[0];
     const reader = new FileReader();
     reader.readAsDataURL(file);
 
-    return new Promise((resolve) => { 
-        reader.onload = () => {	
-          setImageSrc(reader.result || null); // 파일의 컨텐츠
-          setSelectedFiles(file);
-            resolve();
-        };
+    return new Promise((resolve) => {
+      reader.onload = () => {
+        setImageSrc(reader.result || null); // 파일의 컨텐츠
+        setSelectedFiles(file);
+        resolve();
+      };
     });
-  }
+  };
 
   // 생성하기 눌렀을때
-  const createResult = () => {
-    // 사진 보내서 결과 받아오기 과정 필요
-        // forData 생성
+  const createResult = async () => {
+    // AI Server 주소 가져오기
+    let apiUrl;
+    const result = await axios({
+      method: "GET",
+      url: "https://urnotweak.site:8589/aiurl",
+      responseType: "type",
+    });
+    apiUrl = result.data;
+    apiUrl = apiUrl + "/AI";
+    console.log("AI Server 주소 : ", apiUrl);
+
+    // forData 생성
     const formData = new FormData();
-    // Todo : api로 주소 받아와야함
-    let apiUrl = `http://835e-34-23-71-131.ngrok-free.app`
-    const url = apiUrl+"/AI"
-    formData.append('file', selectedFiles);
+    formData.append("file", selectedFiles);
 
-        axios({
-          method: 'POST',
-          url: url,
-          headers: {
-            'Content-Type': 'multipart/form-data', // 이것 필수
+    // AI Server로 업로드한 이미지 전송
+    axios({
+      method: "POST",
+      url: apiUrl,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      data: formData,
+      responseType: "blob",
+    })
+      .then((response) => {
+        const result = URL.createObjectURL(response.data);
+        navigate("/ai/result", {
+          state: {
+            aiImg: result,
           },
-          data: formData,
-          responseType: "blob",
-        }).then((response) => {
-          console.log(response);
-          const result = URL.createObjectURL(response.data)
-          navigate('/ai/result', {
-            state: {
-              aiImg: result,
-            }
-          });
         });
-
-    // navigate('/ai/result');
-  }
-
+      })
+      .catch((Error) => {
+        console.log(Error);
+      });
+  };
 
   return (
     <div className="upload">
       <div>
         <p className="st">예시</p>
-        <ImageSlider/>
+        <ImageSlider />
       </div>
       <div className="split">
         <label htmlFor="file">
           <div className="btn">파일 업로드하기</div>
         </label>
-        <input 
+        <input
           name="file"
           id="file"
-          accept="image/*" 
-          multiple type="file"
-          onChange={e => onUpload(e)}>
-        </input>
-        <div className="btn" onClick={createResult}>사진 생성하기</div>
+          accept="image/*"
+          multiple
+          type="file"
+          onChange={(e) => onUpload(e)}
+        ></input>
+        <div className="btn" onClick={createResult}>
+          사진 생성하기
+        </div>
       </div>
-      <img 
-        className="uploadimg"
-        src={imageSrc} 
-        />
+      <img className="uploadimg" src={imageSrc} />
     </div>
   );
 };
