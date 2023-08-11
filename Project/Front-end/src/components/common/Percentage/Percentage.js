c3e5bacf5ef5e638fead55cbbb67792b46246bc9
soyoon <barcardi26@gmail.com>
2023-08-11 17:29:38 +0900

    Feat : simul - testResult font

--- Project/Front-end/src/components/common/Percentage/Percentage.js
+++ Project/Front-end/src/components/common/Percentage/Percentage.js
@@ -1,36 +1,40 @@
 import React from "react";
+import "./Percentage.css";
 
 export const PercentBar = (props) => {
-    const { bgcolor, completed } = props;
-  
-    const containerStyles = {
-      height: 20,
-      width: '100%',
-      backgroundColor: "#e0e0de",
-      borderRadius: 50,
-    }
-  
-    const fillerStyles = {
-      height: '100%',
-      width: `${completed}%`,
-      backgroundColor: bgcolor,
-      borderRadius: 'inherit',
-      textAlign: 'right'
-    }
-  
-    const labelStyles = {
-      padding: 5,
-      color: 'white',
-      fontWeight: 'bold'
-    }
-  
-    return (
-      <div style={containerStyles}>
-        <div style={fillerStyles}>
-          <span style={labelStyles}>{`${completed}%`}</span>
-        </div>
-      </div>
-    );
+  const { bgcolor, completed } = props;
+
+  const containerStyles = {
+    height: 20,
+    width: "100%",
+    fontSize: "10px",
+    backgroundColor: "#e0e0de",
+    borderRadius: 50,
   };
-  
-  export default PercentBar;
+
+  const fillerStyles = {
+    height: "100%",
+    width: `${completed}%`,
+    backgroundColor: bgcolor,
+    fontSize: "10px",
+    borderRadius: "inherit",
+    textAlign: "right",
+  };
+
+  const labelStyles = {
+    padding: 5,
+    color: "white",
+    fontSize: "10px",
+    fontWeight: "bold",
+  };
+
+  return (
+    <div style={containerStyles}>
+      <div style={fillerStyles}>
+        <span className="percent" style={labelStyles}>{`${completed}%`}</span>
+      </div>
+    </div>
+  );
+};
+
+export default PercentBar;
