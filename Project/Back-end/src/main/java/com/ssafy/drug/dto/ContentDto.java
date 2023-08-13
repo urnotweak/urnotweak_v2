a5dbf0b4cb9b5ffac1e0b5005f073ed4df2d7367
HyoinJeong <hiamy0107@naver.com>
2023-08-14 03:07:35 +0900

    Feat: 컨텐트 카드 가져오기 API

--- Project/Back-end/src/main/java/com/ssafy/drug/dto/ContentDto.java
+++ Project/Back-end/src/main/java/com/ssafy/drug/dto/ContentDto.java
@@ -15,4 +15,12 @@ public class ContentDto {
     int contentLike;
     List<ContentDetailDto> details;
     List<String> tags;
+
+    @Builder
+    public ContentDto(int contentId, String contentType, int contentLike, List<ContentDetailDto> details) {
+        this.contentId = contentId;
+        this.contentType = contentType;
+        this.contentLike = contentLike;
+        this.details = details;
+    }
 }
