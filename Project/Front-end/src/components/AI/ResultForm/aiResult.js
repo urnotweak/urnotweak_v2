d7e044565346d177763e73efe3a4f192ddf85d1f
young eun <duddms0190@naver.com>
2023-08-16 09:36:21 +0900

    Fix : ai upload 파일없을때 막기, 파일 1개만

--- Project/Front-end/src/components/AI/ResultForm/aiResult.js
+++ Project/Front-end/src/components/AI/ResultForm/aiResult.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 // import { BottomMenu } from "components/common/BottomMenu/BottomMenu";
 import { Link } from "react-router-dom";
+import { BottomMenu } from "components/common/BottomMenu/BottomMenu";
+
 import test_img from "../../../assets/images/rec_test.png";
 import ar_img from "../../../assets/images/rec_ar.png";
 import simul_img from "../../../assets/images/rec_.png";
@@ -31,48 +33,10 @@ export const AI = () => {
       <div className="resultimg">
         <img className="uploadimg" src={imageSrc} />
       </div>
-      <div className="btn" onClick={goUpload}>
+      <div className="btn tx-s" onClick={goUpload}>
         다시해보기
       </div>
-      <div className="frame-div222">
-        <div className="group-container222">
-          <div className="group-parent222">
-            <div
-              className="rectangle-group222"
-              // onClick={handleARClick}
-            >
-              <div className="group-item222" />
-              <Link to="/test">
-                <div
-                  className="group-inner222"
-                  style={{ backgroundImage: `url(${test_img})` }}
-                />
-                <div className="ai222">TEST</div>
-              </Link>
-            </div>
-            <div className="rectangle-container222">
-              <div className="group-item222" />
-              <Link to="/ar/filter2">
-                <div
-                  className="group-inner222"
-                  style={{ backgroundImage: `url(${ar_img})` }}
-                />
-              <div className="ai222">AR</div>
-              </Link>
-            </div>
-            <div className="rectangle-parent222">
-              <div className="group-item222" />
-              <Link to="/simul">
-                <div
-                  className="group-inner222"
-                  style={{ backgroundImage: `url(${simul_img})` }}
-                />
-                <div className="ai222">SIMULATION</div>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
+      <BottomMenu></BottomMenu>
     </div>
   );
 };
