0d9eab266255d99ecedc3b1b991b3137917dbb62
soyoon <barcardi26@gmail.com>
2023-08-17 10:39:05 +0900

    Feat : simul - select

--- Project/Front-end/src/pages/Simulation/Simulation.js
+++ Project/Front-end/src/pages/Simulation/Simulation.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import './Simulation.css';
-import { Nav } from 'components/Nav/Nav';
-import SelectForm from 'components/SimulationForm/SelectForm/SelecctForm';
+import React from "react";
+import "./Simulation.css";
+import { Nav } from "components/Nav/Nav";
+import SelectForm from "components/SimulationForm/SelectForm/SelectForm";
 
-import CardForm from 'components/SimulationForm/StoryForm/CardForm';
+import CardForm from "components/SimulationForm/StoryForm/CardForm";
 
 const cards = [
-  'https://ssafy-e204-bucket.s3.ap-northeast-2.amazonaws.com/E204/card/card1.png',
-  'https://ssafy-e204-bucket.s3.ap-northeast-2.amazonaws.com/E204/card/card2.png',
-  'https://ssafy-e204-bucket.s3.ap-northeast-2.amazonaws.com/E204/card/card3.png',
-  'https://ssafy-e204-bucket.s3.ap-northeast-2.amazonaws.com/E204/card/card4.png',
-  'https://ssafy-e204-bucket.s3.ap-northeast-2.amazonaws.com/E204/card/card5.png',
+  "https://ssafy-e204-bucket.s3.ap-northeast-2.amazonaws.com/E204/card/card1.png",
+  "https://ssafy-e204-bucket.s3.ap-northeast-2.amazonaws.com/E204/card/card2.png",
+  "https://ssafy-e204-bucket.s3.ap-northeast-2.amazonaws.com/E204/card/card3.png",
+  "https://ssafy-e204-bucket.s3.ap-northeast-2.amazonaws.com/E204/card/card4.png",
+  "https://ssafy-e204-bucket.s3.ap-northeast-2.amazonaws.com/E204/card/card5.png",
 ];
 
 export const Simulation = () => {
