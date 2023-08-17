48d36ed915a8eeebe6f894b65179d93337ee0a94
youngeun <duddms0190@naver.com>
2023-08-17 21:01:31 +0900

    Fix: home 이상해...

--- Project/Front-end/src/components/App/App.js
+++ Project/Front-end/src/components/App/App.js
@@ -1,44 +1,51 @@
-import React, {lazy, Suspense} from "react";
+import React, {useEffect} from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-
-// // import Home from "pages/Homepage/HomePage"
-// import Simulation from "pages/Simulation/Simulation"
-// import AI from 'pages/AI/AI'
-// import AR from 'pages/AR/AR'
-// import Test from 'pages/Test/Test'
-// import NotFound from 'pages/NotFound/NotFound'
-
-
-// import Content from 'pages/Content/Content'
+import Home from "pages/Homepage/HomePage"
+import Simulation from "pages/Simulation/Simulation"
+import AI from 'pages/AI/AI'
+import AR from 'pages/AR/AR'
+import Test from 'pages/Test/Test'
+import NotFound from 'pages/NotFound/NotFound'
+import Content from 'pages/Content/Content'
 import 'fonts/font.css'
-import Loading from 'pages/Loading/Loading';
 
-const Simulation = lazy(() => import('pages/Simulation/Simulation'));
-const AI = lazy(() => import('pages/AI/AI'));
-const AR = lazy(() => import('pages/AR/AR'));
-const Test = lazy(() => import('pages/Test/Test'));
-const Content = lazy(() => import('pages/Content/Content'));
-const Home = lazy(() => import('pages/Homepage/HomePage'));
-const NotFound = lazy(() => import('pages/NotFound/NotFound'));
 
 function App() {
+
+  useEffect(() => {
+    
+    function setScreenSize() {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+    setScreenSize();
+
+    // 높이가 변경되었을때 다시 정의
+    window.addEventListener('resize', () => setScreenSize());
+  }, []);
+
   return (
-    <div className="display">
-      <Router>
-        <Suspense fallback={<Loading />}>
+    <div>
+      <div className="display">
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="simul" element={<Simulation />} />
-            <Route path="ai/*" element={<AI />} />
+            <Route path="ai/*" element={<AI/> } />
             <Route path="ar/*" element={<AR />} />
             <Route path="test/*" element={<Test />} />
-            <Route path="content/*" element={<Content />} />
+            <Route path="content/*" element={<Content/>} />
             <Route path="/*" element={<NotFound />} />
           </Routes>
-        </Suspense>
-      </Router>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
