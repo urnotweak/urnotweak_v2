7b25ca105306fb56c3346cdcc024bc03c9963b22
youngeun <duddms0190@naver.com>
2023-08-17 20:48:22 +0900

    Fix: lazy로 코드 스플릿

---
+++ Project/Front-end/src/pages/Loading/Loading.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import './Loading.css'
+
+import thumb1 from 'assets/images/contentThumnail01.webp';
+import thumb2 from 'assets/images/contentThumnail02.webp';
+import thumb3 from 'assets/images/contentThumnail03.webp';
+
+export const Loading = () => {
+  const images = [thumb1, thumb2, thumb3]; // 이미지 배열
+  const [currentImageIndex, setCurrentImageIndex] = useState(0); // 현재 이미지 인덱스
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // 다음 이미지 인덱스로 업데이트
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 700); // 0.7초마다 이미지 변경
+
+    return () => {
+      clearInterval(interval); // 컴포넌트 언마운트 시 인터벌 해제
+    };
+  }, []);
+
+  return (
+    <div className="loading">
+      <div className="wrapper">
+        <img className="small-img" src={images[currentImageIndex]} alt={`Image ${currentImageIndex}`} />
+        <p className="tx-s">로딩중입니다.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
